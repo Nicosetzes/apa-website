23a49dee2e3eba91ae1ccbd7c4b7b236774a006f
unknown <nicosetzes@gmail.com>
2022-04-23 21:36:26 -0300

    V2.3 I've worked on the frontend, on the main page. I have to fix a weird overflow that appeared on main.

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,67 @@
 // MODALS
 
-document.addEventListener("click", e => {
-  let modalCloseButton;
-  e.target.matches(".button-modal-close") ? modalCloseButton = e.target : modalCloseButton = e.target.closest(".button-modal-close");
-  if (modalCloseButton != null) onModalCloseButtonClick(modalCloseButton);
-}, false);
+document.addEventListener(
+  "click",
+  (e) => {
+    let modalCloseButton;
+    e.target.matches(".button-modal-close")
+      ? (modalCloseButton = e.target)
+      : (modalCloseButton = e.target.closest(".button-modal-close"));
+    if (modalCloseButton != null) onModalCloseButtonClick(modalCloseButton);
+  },
+  false
+);
 
-document.addEventListener("click", e => {
-  let containerModal;
-  e.target.matches(".container__tournament-content-champion") ? containerModal = e.target : containerModal = e.target.closest(".container__tournament-content-champion");
-  if (containerModal != null) onContainerModalClick(containerModal);
-}, false);
+document.addEventListener(
+  "click",
+  (e) => {
+    let containerModal;
+    e.target.matches(".container__tournament-content-plaque")
+      ? (containerModal = e.target)
+      : (containerModal = e.target.closest(
+          ".container__tournament-content-plaque"
+        ));
+    if (containerModal != null) onContainerModalClick(containerModal);
+  },
+  false
+);
 
-document.addEventListener("click", e => {
-  let containerModal;
-  e.target.matches(".container__tournament-content-finalist") ? containerModal = e.target : containerModal = e.target.closest(".container__tournament-content-finalist");
-  if (containerModal != null) onContainerModalClick(containerModal);
-}, false);
+// document.addEventListener(
+//   "click",
+//   (e) => {
+//     let containerModal;
+//     e.target.matches(".container__tournament-content-finalist")
+//       ? (containerModal = e.target)
+//       : (containerModal = e.target.closest(
+//           ".container__tournament-content-finalist"
+//         ));
+//     if (containerModal != null) onContainerModalClick(containerModal);
+//   },
+//   false
+// );
 
 const onContainerModalClick = (containerModal) => {
-  const dividedArray = containerModal.classList[0].split("-");
+  const dividedArray = containerModal.classList[1].split("-");
+  console.log(dividedArray);
   const isChampionOrFinalist = dividedArray[dividedArray.length - 1];
-  if ((isChampionOrFinalist !== "champion") && (isChampionOrFinalist !== "finalist")) throw new Error({ error: "isChampionOrFinalist is neither of both" })
-  const modal = containerModal.closest(".container__tournament-content").querySelector(`.modal-profile-${isChampionOrFinalist}`);
+  if (
+    isChampionOrFinalist !== "champion" &&
+    isChampionOrFinalist !== "finalist"
+  )
+    throw new Error({ error: "isChampionOrFinalist is neither of both" });
+  const modal = containerModal
+    .closest(".container__tournament-content")
+    .querySelector(`.modal-profile-${isChampionOrFinalist}`);
+  const containerTournamentTitle = containerModal
+    .closest(".active")
+    .querySelector(".container__tournament-title");
+  const imgTrophy = containerModal
+    .closest(".active")
+    .querySelector(".img-trophy");
   if (modal.classList.contains("hidden")) {
     modal.classList.remove("hidden");
+    containerTournamentTitle.classList.add("no-index");
+    imgTrophy.classList.add("no-index");
     setTimeout(() => {
       modal.classList.remove("visuallyHidden");
     }, 20);
@@ -33,12 +70,22 @@ const onContainerModalClick = (containerModal) => {
 
 const onModalCloseButtonClick = (modalCloseButton) => {
   const modal = modalCloseButton.closest(".modal-profile");
+  const containerTournamentTitle = modal
+    .closest(".active")
+    .querySelector(".container__tournament-title");
+  const imgTrophy = modal.closest(".active").querySelector(".img-trophy");
   modal.classList.add("visuallyHidden");
-  modal.addEventListener("transitionend", (e) => {
-    modal.classList.add("hidden");
-  }, {
-    capture: false,
-    once: true, // Without this, animations breaks. 
-    passive: false
-  });
-}
+  containerTournamentTitle.classList.remove("no-index");
+  imgTrophy.classList.remove("no-index");
+  modal.addEventListener(
+    "transitionend",
+    (e) => {
+      modal.classList.add("hidden");
+    },
+    {
+      capture: false,
+      once: true, // Without this, animations breaks.
+      passive: false,
+    }
+  );
+};
